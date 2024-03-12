be0a8aaf68109ee1ced1c0e3012ccb6890bf6416
pet1412 <162765300+pet1412@users.noreply.github.com>
2024-03-12 14:25:54 +0200

    доробив інтерфейс на 2 стани

--- app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
@@ -1,11 +1,14 @@
 package ua.edu.lntu.cw_2
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_2.ui.theme.IPZ_CW_2_PetlenkoTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +40,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    UiLogin()
+    SingInSuccess("fsdfsdf")
 }
 
 @Preview(showBackground = true)
@@ -48,16 +52,35 @@ fun GreetingPreview() {
 }
 
 @Composable
-fun UiLogin(){
+fun SingIn(){
 
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Login")
-        TextField(value = "Hello", onValueChange = {} )
+        Spacer(modifier = Modifier.height(12.dp))
+        TextField(value = "Email", onValueChange = {} )
+        Spacer(modifier = Modifier.height(12.dp))
+        TextField(value = "Password", onValueChange = {} )
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            onClick = { /*TODO*/ }) {
+            Text(text = "Sign In")
+        }
+    }
+}
+
+@Composable
+fun SingInSuccess(email: String){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Sing In Success")
+        Text(text = email)
         Button(onClick = { /*TODO*/ }) {
-            
+                Text(text = "Sing out")
         }
     }
 }
