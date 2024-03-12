1b98c6c570dd5704d1c1fcc7a8251af715c015c0
pet1412 <162765300+pet1412@users.noreply.github.com>
2024-03-12 14:16:25 +0200

    створив інтерфейс

--- app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
@@ -3,11 +3,16 @@ package ua.edu.lntu.cw_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw_2.ui.theme.IPZ_CW_2_PetlenkoTheme
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +35,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    UiLogin()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IPZ_CW_2_PetlenkoTheme {
-        Greeting("Android")
+        Greeting()
+    }
+}
+
+@Composable
+fun UiLogin(){
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login")
+        TextField(value = "Hello", onValueChange = {} )
+        Button(onClick = { /*TODO*/ }) {
+            
+        }
     }
 }
